b30b47ad19e96fdb1d16933dd5ccf047dd243b3f
Henry J. Krumb <mail@henrykrumb.de>
2025-12-10 13:17:47 +0100

    create user config if not available yet

--- kvasircapsuleloader/config.py
+++ kvasircapsuleloader/config.py
@@ -3,10 +3,19 @@ from pathlib import Path
 
 PROJECT_PATH = Path(__file__).parent.parent
 
+# Create user config if not available yet
+USER_CONFIG_PATH = Path("~").expanduser() / ".kvasircapsuleloader.json"
+if not USER_CONFIG_PATH.exists():
+    with open(USER_CONFIG_PATH, "w") as f:
+        json.dump({}, f)
+
+# Config hierarchy, bottom overwrites top
 CONFIG_PATHS = [
     PROJECT_PATH / "config.json",
-    Path("~").expanduser() / ".kvasircapsuleloader.json",
+    USER_CONFIG_PATH,
 ]
+
+# Load config
 CONFIG = {}
 for path in CONFIG_PATHS:
     if not path.exists():
@@ -14,6 +23,7 @@ for path in CONFIG_PATHS:
     with open(path, "r") as f:
         CONFIG.update(json.load(f))
 
+# Load and process config values
 KVASIR_CAPSULE_PATH = Path(CONFIG["kvasir-capsule-path"]).expanduser()
 KVASIR_CAPSULE_PATH.mkdir(exist_ok=True, parents=True)
 DEFAULT_RANDOM_SEED = CONFIG.get("random-seed", 1337)
